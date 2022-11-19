fc4749834571741b504f653146dfa37e09ffbc64
heyuxin <heyux1n@foxmail.com>
2022-11-19 17:41:10 +0800

    Initial commit

---
+++ mall-coupon/src/main/java/cn/unnow/mallcoupon/MallCouponApplication.java
@@ -0,0 +1,13 @@
+package cn.unnow.mallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCouponApplication.class, args);
+    }
+
+}
