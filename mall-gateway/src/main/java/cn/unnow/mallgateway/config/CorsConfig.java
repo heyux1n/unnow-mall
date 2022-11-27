f8b1d2d0d4056ff4388d77e3c89994c7fee612d8
heyuxin <heyux1n@foxmail.com>
2022-11-27 20:44:37 +0800

    feat(项目配置): gateway模块增加跨域配置

---
+++ mall-gateway/src/main/java/cn/unnow/mallgateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package cn.unnow.mallgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author: heyuxin
+ * @Date: 2022/11/27 19:54
+ * @Version: 1.0
+ * @Description:
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
